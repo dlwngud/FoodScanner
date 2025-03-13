d9a04d02ddc4b9cf8b79b9d0ab2d58688a988886
이주형 <leewngud980411@gmail.com>
2025-03-14 01:26:47 +0900

    [chore] 카메라, 권한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,14 @@ dependencies {
 
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.mlkit.vision)
+    implementation(libs.androidx.camera.extensions)
+    implementation(libs.androidx.camera.compose)
+
+    implementation(libs.accompanist.permissions)
 }
